49932cb87ba40ca28c63d52845aba37c78683e60
Jzcoob Gamer <jzcoob@gmail.com>
2022-09-21 13:24:53 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+token='OTI2MTYzMjY5NTAzMjk5Njk1.G5xucN.PuTzzRakq3seI5BztyJhbeWgmG1214AP9lGNL8'
+truth='https://api.truthordarebot.xyz/v1/truth'
+dare='https://api.truthordarebot.xyz/v1/dare'
+wyr='https://api.truthordarebot.xyz/v1/wyr'
+nhie='https://api.truthordarebot.xyz/v1/nhie'
+paranoia='https://api.truthordarebot.xyz/v1/paranoia'
+
+
+host='na01-sql.pebblehost.com'
+user='customer_249998_fuyu'
+password='TsWjN1@Ni9#$BigPrkfP'
+db='customer_249998_fuyu'
