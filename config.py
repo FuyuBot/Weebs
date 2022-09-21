03e0b0836b1e60328493f13cf132ab3cc497f6f0
Jzcoob Gamer <jzcoob@gmail.com>
2022-09-21 13:22:04 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-token = 'OTI2MTYzMjY5NTAzMjk5Njk1.Yc3qwg.mi_SxMzR0YN5wPOPoSOtw062Sz4'
+token = 'OTI2MTYzMjY5NTAzMjk5Njk1.G5xucN.PuTzzRakq3seI5BztyJhbeWgmG1214AP9lGNL8'
 truth = 'https://api.truthordarebot.xyz/v1/truth'
 dare = 'https://api.truthordarebot.xyz/v1/dare'
 wyr = 'https://api.truthordarebot.xyz/v1/wyr'
