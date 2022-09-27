d6aa5e8e3bf29cff78faa78b710ab6cd25b6755f
Jzcoob Gamer <jzcoob@gmail.com>
2022-09-27 13:05:24 -0400

    a little logs

--- cogs/logs.py
+++ cogs/logs.py
@@ -1,3 +1,4 @@
+from re import T
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -12,16 +13,27 @@ class logs(commands.Cog):
         print("LOADED: `logs.py`")
     
     @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        try:
+            embed = discord.Embed(color=0x2699C6)
+            embed.set_author(name=message.author, icon_url=message.author.avatar)
+            embed.add_field(name=f'Messaged Deleted in #{message.channel.name}', value=message.content)
+            embed.set_footer(text=f"User's ID: {message.author.id}")
+            logsChannel = self.bot.get_channel(865073643553423360)
+            await logsChannel.send(embed=embed)
+        except Exception as e:
+            print(e)
+    
+    @commands.Cog.listener()
     async def on_message_edit(self, before, after):
         try:
-            embed = discord.Embed(
-                title=f"edited their message.",
-                color=0x2699C6
-            )
-            embed.add_field(name='Before', value=before)
-            embed.add_field(name='After', value=after)
-            logsChannel = self.bot.get_channel(865073673668526080)
-            #await logsChannel.send(embed=embed)
+            embed = discord.Embed(title=f'Messaged Edited in #{before.channel.name}',color=0x2699C6)
+            embed.set_author(name=before.author, icon_url=before.author.avatar)
+            embed.add_field(name=f'Before', value=before.content)
+            embed.add_field(name=f'After', value=after.content)
+            embed.set_footer(text=f"User's ID: {before.author.id}")
+            logsChannel = self.bot.get_channel(865073643553423360)
+            await logsChannel.send(embed=embed)
         except Exception as e:
             print(e)
 
