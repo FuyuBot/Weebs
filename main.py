9cd758acc23b32ef374b7b14b4de0af867c8a2d6
Jzcoob Gamer <jzcoob@gmail.com>
2022-11-28 13:37:44 -0500

    final update before db rework.

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ intents.message_content = True
 intents.auto_moderation_configuration = True
 intents.reactions = True
 bot = commands.Bot(command_prefix='.', intents=intents)
+status = discord.Status.online
+
 
 
 @bot.event
@@ -28,6 +30,9 @@ async def load():
             
 async def main():
     await load()
+    game = discord.Game("A Weeb's Hangout")
+    #await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="A Weeb's Hangout"))
+    #await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="A Weeb's Hangout"))
     await bot.start(config.token)
 
 asyncio.run(main())
