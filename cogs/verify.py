b30466a1624a53bf63158b3bac16a9f5211598dd
Jzcoob Gamer <jzcoob@gmail.com>
2022-12-06 22:40:36 -0500

    fixed verify?

--- cogs/verify.py
+++ cogs/verify.py
@@ -27,66 +27,69 @@ class verfiy(commands.Cog):
     @app_commands.command(name='force-verify', description='Allows Administrators to forcibly verify someone.')
     @app_commands.checks.has_any_role("Administrator", "Management Team")
     async def forceverify(self, interaction:discord.Interaction, user: discord.Member):
-        unverified = discord.utils.get(user.guild.roles, name="unverified")
-        if unverified  in user.roles:
-            verified = discord.utils.get(user.guild.roles, name="Member")
-            logsChannel = interaction.client.get_channel(865073673668526080)
-            welcomeChannel = interaction.client.get_channel(865056995295625256)
+        try:
+            unverified = discord.utils.get(user.guild.roles, name="unverified")
+            if unverified  in user.roles:
+                verified = discord.utils.get(user.guild.roles, name="Member")
+                logsChannel = interaction.client.get_channel(865073673668526080)
+                welcomeChannel = interaction.client.get_channel(865056995295625256)
 
-            logsEmbed = discord.Embed(
-                title=f'{user} Verified.',
-                color=config.color
-            )
-            logsEmbed.set_footer(text=f"ID: {user.id}")
-            DMembed = discord.Embed(
-                title=f'You were forcibly verified by an Administrator of the server. Welcome to the server.',
+                logsEmbed = discord.Embed(
+                    title=f'{user} Verified.',
+                    color=config.color
+                )
+                logsEmbed.set_footer(text=f"ID: {user.id}")
+                DMembed = discord.Embed(
+                    title=f'You were forcibly verified by an Administrator of the server. Welcome to the server.',
+                    color=config.color,
+                    description=":loudspeaker: Please follow all the rules and have a great time here.\n\
+                    In order to get DM's from our bot <@926163269503299695> please allow DM's on this server.\n\n\
+                    Check out <#892554752175517756> to get as many roles as you would want.\n\
+                    <#915078396990603296> has a lot of the information for the server."
+                )
+                embed = discord.Embed(
+                title=f'Welcome {user} to the server.',
                 color=config.color,
                 description=":loudspeaker: Please follow all the rules and have a great time here.\n\
-                In order to get DM's from our bot <@926163269503299695> please allow DM's on this server.\n\n\
-                Check out <#892554752175517756> to get as many roles as you would want.\n\
-                <#915078396990603296> has a lot of the information for the server."
-            )
-            embed = discord.Embed(
-            title=f'Welcome {user} to the server.',
-            color=config.color,
-            description=":loudspeaker: Please follow all the rules and have a great time here.\n\
-                In order to get DM's from our bot <@926163269503299695> please allow DM's on this server.\n\n\
-                Check out <#892554752175517756> to get as many roles as you would want.\n\
-                <#915078396990603296> has a lot of the information for the server."
-            )
-            mydict = { 
-                "_id": interaction.user.id,
-                "info": {
-                    "joined": interaction.user.joined_at,
-                    "applied": False,
-                    "currently_banned": False
-                },
-                "levels": {
-                    "level": 0,
-                    "xp": 0
-                },
-                "economy": {
-                    "wallet": 0,
-                    "bank": 0
-                },
-                "punishments": {
-                    "bans": [],
-                    "timeouts": [],
-                    "warns": [],
-                    "notes": []
+                    In order to get DM's from our bot <@926163269503299695> please allow DM's on this server.\n\n\
+                    Check out <#892554752175517756> to get as many roles as you would want.\n\
+                    <#915078396990603296> has a lot of the information for the server."
+                )
+                mydict = { 
+                    "_id": user.id,
+                    "info": {
+                        "joined": user.joined_at,
+                        "applied": False,
+                        "currently_banned": False
+                    },
+                    "levels": {
+                        "level": 0,
+                        "xp": 0
+                    },
+                    "economy": {
+                        "wallet": 0,
+                        "bank": 0
+                    },
+                    "punishments": {
+                        "bans": [],
+                        "timeouts": [],
+                        "warns": [],
+                        "notes": []
+                    }
                 }
-            }
-            
-            mycol.insert_one(mydict)
+                
+                mycol.insert_one(mydict)
 
-            await user.add_roles(verified)
-            await user.remove_roles(unverified)
-            await logsChannel.send(embed=logsEmbed)
-            await user.send(embed=DMembed)
-            await welcomeChannel.send(embed=embed)
-            await interaction.response.send_message(f"{user} was verified successfully.")
-        else:
-            await interaction.response.send_message(f"That user is already verified.")
+                await user.add_roles(verified)
+                await user.remove_roles(unverified)
+                await logsChannel.send(embed=logsEmbed)
+                await user.send(embed=DMembed)
+                await welcomeChannel.send(embed=embed)
+                await interaction.response.send_message(f"{user} was verified successfully.")
+            else:
+                await interaction.response.send_message(f"That user is already verified.")
+        except Exception as e:
+            print(e)        
 
 class VerifyModal(discord.ui.Modal, title="Verify"):
     verification = discord.ui.TextInput(label="Verification", placeholder="Enter your Name and Tag here. e.g. Name#1234", required=True)
