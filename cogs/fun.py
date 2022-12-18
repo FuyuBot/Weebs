c9ea087d39d1b84a87732f2853d3c8f54223329f
Jzcoob Gamer <jzcoob@gmail.com>
2022-12-18 16:44:09 -0500

    added bot info

--- cogs/fun.py
+++ cogs/fun.py
@@ -12,7 +12,25 @@ class fun(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print('LOADED: `fun.py`')
-
+    
+    @app_commands.command(name="bot-info", description="Displays information about the bot.")
+    async def botInfo(self, interaction: discord.Interaction):
+        try:
+            client = interaction.client
+            embed = discord.Embed(color=config.color, title=f"{client.user}'s Information", description=f"\
+                Bot's Creators: Jzcob#2842, Sezn#6554, and SpaceZ#4346\n\
+                Questions Commands: https://docs.truthordarebot.xyz/api-docs\n\
+                Cat Command: https://developers.thecatapi.com/\n\
+                Dog Command: https://dog.ceo/dog-api/\n\
+                Duck Command: https://random-d.uk/api\n\
+                Fox Command: https://randomfox.ca/\n\
+                Lucifier Command: https://lucifer-quotes.vercel.app/\n\
+                Anime Commands: https://otakugifs.xyz/api\n\n\
+                Bot is coded in `Python` v3.10.5 and using `discord.py` v2.1.0")
+            embed.set_thumbnail(url=client.user.avatar)
+            await interaction.response.send_message(embed=embed)
+        except Exception as e:
+            print(e)
     
     @app_commands.command(name="lucifer-quote", description="Sends a quote from the show Lucifer.")
     async def lucifer(self, interaction: discord.Interaction):
