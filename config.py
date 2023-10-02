e6ff380984a46d0298f716f2bf24d01dc503916f
Jacob <50559088+Jzcob@users.noreply.github.com>
2023-10-02 18:49:59 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-token='OTI2MTYzMjY5NTAzMjk5Njk1.G5xucN.PuTzzRakq3seI5BztyJhbeWgmG1214AP9lGNL8'
 truth='https://api.truthordarebot.xyz/v1/truth'
 dare='https://api.truthordarebot.xyz/v1/dare'
 wyr='https://api.truthordarebot.xyz/v1/wyr'
