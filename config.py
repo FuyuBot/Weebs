b247159548206b266be1682f568c09efd877df89
Jzcoob Gamer <jzcoob@gmail.com>
2023-10-08 18:50:13 -0400

    removed mongo db thing

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ weebsHangout = 860752406551461909
 
 openWeatherApiKEY = "66a1612e83d41045b58f7853e726c5a7"
 
-mongoDB = "mongodb+srv://bot:bc2v5zDu@cluster0.uch3p.mongodb.net"
+
 
 errorChannel = 1072917579234033815
 
